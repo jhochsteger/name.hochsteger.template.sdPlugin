5c3d223c698704ca6b1c48435e117172a3b56f5c
jhochsteger <jhochsteger@student.tgm.ac.at>
2023-02-06 23:05:43 +0100

    Updated the name of the pluginObserver theoretically functional code for the PropertyInspector added the css file for the property inspector

--- pi/piObserver.js
+++ pi/piObserver.js
@@ -1,4 +1,4 @@
-class Observer { // Copy this class for each action you have in your plugin
+class PiObserver { // Copy this class for each action you have in your plugin
     update(event, jsonEvent) {
         switch (event) { // Don't forget there is ReceiveEvents
             case "the event you need": // TODO change this to the event you need
@@ -11,6 +11,6 @@ class Observer { // Copy this class for each action you have in your plugin
     }
 
     constructor() {
-        streamDeckSocketInstance.subscribeToEvent("the event you need", this); // TODO change this to the event you need
+        streamDeckSocketPIInstance.subscribeToEvent("the event you need", this); // TODO change this to the event you need
     }
 }
